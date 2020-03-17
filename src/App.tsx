793225a9239c7f9dfe32899d196e5801cb743a95
Salomao Rodrigues <salomao88@gmail.com>
2020-03-17 23:21:33 +0000

    Add initial page with list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+
+type Cases = {
+  confirmed: string
+  country: string
+  province: string
 }
 
-export default App;
+const COVID_TOTALS = gql`
+  query {
+    casesByLocation {
+      confirmed
+      country
+      province
+    }
+    totalCases {
+      totalConfirmed,
+      totalRecovered,
+      totalDeaths,
+    }
+  }
+`;
+
+const App: React.FC = () => {
+  const { loading, error, data } = useQuery(COVID_TOTALS)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>{JSON.stringify(error, null, 2)}</p>
+
+  return (
+    <div>
+      <h2>My first Apollo app <span role="img" aria-label="image">🚀</span></h2>
+      <div className='container'>
+      <ul>
+        {data.casesByLocation.map(({ country, province, confirmed }: Cases, index: number) => (
+          <li key={index}>{country} - {province}: {confirmed}</li>
+        ))}
+      </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
