6594300c1921abb311e5a745c456e989fe4bfd9c
Salomao Rodrigues <salomao88@gmail.com>
2020-03-20 12:33:01 +0000

    Add Global Time series graph

--- src/App.jsx
+++ src/App.jsx
@@ -4,53 +4,68 @@ import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 
 import './App.css'
+import TimeSeries from './components/TimeSeries'
 
 const COVID_TOTALS = gql`
+  fragment CaseFields on Cases {
+    confirmed
+    recovered
+    deaths
+    active
+  }
   query {
     totalCases {
-      confirmed
-      recovered
-      deaths
-      active
+      ...CaseFields
+    }
+    globalTimeSeries {
+      ...CaseFields
+      day
     }
   }
 `
 
-const App = () => {
+const App = ({ totalCases, globalTimeSeries }) => (
+  <div>
+    <div className="container-xl">
+      <h3>COVID-19 global cases by day</h3>
+    </div>
+    <br></br>
+    <div className="container-xl">
+      <div className="row">
+        <div className="col-sm">
+          <div className="alert alert-danger" role="alert">
+            <h5>Total confirmed</h5>
+            <div id="confirmedCounter" className="total-cases text-danger">{ totalCases.confirmed.toLocaleString() }</div>
+          </div>
+        </div>
+        <div className="col-sm">
+          <div className="alert alert-primary" role="alert">
+            <h5>Total active</h5>
+            <div id="activeCounter" className="total-cases text-primary">{ totalCases.active.toLocaleString() }</div>
+          </div> 
+        </div>
+        <div className="col-sm">
+          <div className="alert alert-success" role="alert">
+            <h5>Total recovered</h5>
+            <div id="recoveredCounter" className="total-cases text-success">{ totalCases.recovered.toLocaleString() }</div>
+          </div>
+        </div>
+        <div className="col-sm">
+          <div className="alert alert-dark" role="alert">
+            <h5>Total deaths</h5>
+            <div id="deathsCounter" className="total-cases text-dark">{ totalCases.deaths.toLocaleString() }</div>
+          </div>
+        </div>
+      </div>
+      <TimeSeries data={globalTimeSeries} />
+    </div>
+  </div>
+)
+
+export default () => {
   const { loading, error, data } = useQuery(COVID_TOTALS)
   if (loading) return <p>Loading...</p>
   if (error) return <p>{JSON.stringify(error, null, 2)}</p>
 
-  return (
-    <div>
-      <div className='container'>
-        <h1>Coronavirus COVID-19</h1>
-      </div>
-      <div className='container'>
-        <div className='row'>
-          <div className="col-sm">
-            <h3>Total Confirmed</h3>
-            <div className='total-cases text-danger'>{data.totalCases.confirmed.toLocaleString()}</div>
-          </div>
-          <div className="col-sm">
-            <h3>Total Deaths</h3>
-            <div className='total-cases text-dark'>{data.totalCases.deaths.toLocaleString()}</div>
-          </div>
-          <div className="col-sm">
-            <h3>Total Recovered</h3>
-            <div className='total-cases text-success'>{data.totalCases.recovered.toLocaleString()}</div>
-          </div>
-        </div>
-      </div>
-      <div className='container'>
-      <ul>
-        {/* {data.casesByLocation.map(({ country, province, confirmed }: Cases, index: number) => (
-          <li key={index}>{country} - {province}: {confirmed}</li>
-        ))} */}
-      </ul>
-      </div>
-    </div>
-  )
+  return <App {...data} />
 }
-
-export default App
---
+++ src/components/TimeSeries.jsx
@@ -0,0 +1,130 @@
+import React, { useEffect } from 'react'
+import Chart from 'chart.js'
+
+const TimeSeries = ({ data }) => {
+  const chartRef = React.createRef()
+  const allDates = []
+  const confirmed = []
+  const recovered = []
+  const deaths = []
+  const active = []
+
+  const chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
+  }
+  
+  const chartConfig = {
+    type: 'line',
+    data: {
+      labels: allDates,
+      datasets: [
+        {
+        label: 'Confirmed',
+        backgroundColor: chartColors.red,
+        borderColor: chartColors.red,
+        data: confirmed,
+        fill: false,
+      }, 
+      {
+        label: 'Active',
+        fill: false,
+        backgroundColor: chartColors.blue,
+        borderColor: chartColors.blue,
+        data: active,
+      }, 
+      {
+        label: 'Recovered',
+        fill: false,
+        backgroundColor: chartColors.green,
+        borderColor: chartColors.green,
+        data: recovered,
+      }, 
+      {
+        label: 'Deaths',
+        fill: false,
+        backgroundColor: chartColors.grey,
+        borderColor: chartColors.grey,
+        data: deaths,
+      }
+    ]
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Covid-19 cases by day'
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      scales: {
+        xAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Date'
+          }
+        }],
+        yAxes: [{
+          display: true,
+          type: 'linear',
+          scaleLabel: {
+            display: true,
+            labelString: 'Number of cases'
+          },
+          ticks: {
+            beginAtZero: true,
+            callback: function(value, index, values) {
+                return value.toLocaleString()
+            }
+          }
+        }]
+      }
+    }
+  }
+
+  useEffect(() => {
+    const myChartRef = chartRef.current.getContext("2d")
+
+    new Chart(myChartRef, chartConfig);
+  }, [chartRef, chartConfig])
+  
+  data.forEach(element => {
+    const dateFromString = new Date(element.day)
+    allDates.push((dateFromString).toLocaleDateString())
+    confirmed.push({x: dateFromString, y: element.confirmed})
+    recovered.push({x: dateFromString, y: element.recovered})
+    deaths.push({x: dateFromString, y: element.deaths})
+    active.push({x: dateFromString, y: element.active})
+  })
+  
+  return (
+    <div className="container-xl">
+      <div id="chart">
+        <canvas
+          id="canvas"
+          ref={chartRef}
+        ></canvas>
+      </div>
+      <p className="lastUpdatedTimeStamp">Data last updated: <span id="lastUpdated"></span></p>
+      <button type="button" className="btn btn-sm btn-primary" id="changeScale">View logarithmic</button>
+      <button type="button" className="btn btn-sm btn-primary" id="changeChartType">View bar chart</button>
+      <button type="button" className="btn btn-sm btn-secondary" id="toggleCurrentCases">Add current cases</button>
+      <br />
+      <br/>
+    </div>
+  )
+}
+
+export default TimeSeries
