0268600538a2916b8306e6ea027148177ec8723e
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-03-22 18:20:12 +0000

    Moved inner page into a seperate componant

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 
 import './App.css'
-import TimeSeries from './components/TimeSeries'
-import TopXBarGraph from './components/TopXBarGraph'
-// import PieChart from './components/PieChart'
-import ProgressBar from './components/ProgressBar'
+import InnerPage from './components/InnerPage'
 
 const COVID_TOTALS = gql`
   fragment CaseFields on Cases {
@@ -112,101 +109,17 @@ const App = (
         </nav>
 
         <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-          <div className="">
-            <h3>COVID-19 | Global</h3>
-          </div>
-          <br></br>
-          <div className="">
-            <div className="row">
-              <div className="col-sm">
-                <div className="alert alert-danger" role="alert">
-                  <h5>Total confirmed</h5>
-                  <div id="confirmedCounter" className="total-cases text-danger">{ totalCases.confirmed.toLocaleString() }</div>
-                </div>
-              </div>
-              <div className="col-sm">
-                <div className="alert alert-primary" role="alert">
-                  <h5>Total active</h5>
-                  <div id="activeCounter" className="total-cases text-primary">{ totalCases.active.toLocaleString() }</div>
-                </div> 
-              </div>
-              <div className="col-sm">
-                <div className="alert alert-success" role="alert">
-                  <h5>Total recovered</h5>
-                  <div id="recoveredCounter" className="total-cases text-success">{ totalCases.recovered.toLocaleString() }</div>
-                </div>
-              </div>
-              <div className="col-sm">
-                <div className="alert alert-dark" role="alert">
-                  <h5>Total deaths</h5>
-                  <div id="deathsCounter" className="total-cases text-dark">{ totalCases.deaths.toLocaleString() }</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="">
-            <div className="row">
-              <div className="col-sm">
-                <ProgressBar
-                  data1={totalCases.confirmed}
-                  data2={totalCases.active}
-                  id="barConfirmedAndActive"
-                  chartLabel1="confirmed"
-                  chartLabel2="active"
-                />
-              </div>
-              <div className="col-sm">
-                <ProgressBar
-                  data1={totalCases.recovered}
-                  data2={totalCases.deaths}
-                  id="barRecoveredAndDeaths"
-                  chartLabel1="recovered"
-                  chartLabel2="deaths"
-                />
-              </div>
-            </div>
-            <div className="row">
-              {/* <PieChart
-                data1={totalCases.confirmed}
-                data2={totalCases.active}
-                id="pieConfirmedAndActive"
-                chartTitle="Convirmed v Active"
-                chartLabel1="confirmed"
-                chartLabel2="active"
-                labelColor1="red"
-                labelColor2="blue"
-              />
-              <PieChart
-                data1={totalCases.recovered}
-                data2={totalCases.deaths}
-                id="pieRecoveredAndDeaths"
-                chartTitle="Recovered v Deaths"
-                chartLabel1="recovered"
-                chartLabel2="deaths"
-                labelColor1="green"
-                labelColor2="grey"
-              /> */}
-            </div>
-          </div>
-          <TimeSeries lastUpdated={lastUpdated} data={globalTimeSeries} />
-          <div className="">
-            <div className="row">
-              <TopXBarGraph data={topXconfirmedByCountry} id="top5confirmed" chartTitle="Top 5 confirmed by country" chartLabel="confirmed" labelColor="red" />
-              <TopXBarGraph data={topXactiveByCountry} id="top5active" chartTitle="Top 5 active by country" chartLabel="active" labelColor="blue" />
-            </div>
-            <div className="row">
-              <TopXBarGraph data={topXrecoveredByCountry} id="top5recovered" chartTitle="Top 5 recovered by country" chartLabel="recovered" labelColor="green" />
-              <TopXBarGraph data={topXdeathsByCountry} id="top5deaths" chartTitle="Top 5 deaths by country" chartLabel="deaths" labelColor="grey" />
-            </div>
-          </div>
-          <footer className="footer mt-auto py-3">
-            <div className="container pull-left">
-              <span className="text-muted">Data sources: <a href="https://www.who.int/">WHO</a>, <a href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6">John Hopkins University</a></span>
-            </div>
-            <div className="container pull-right">
-              <span className="text-muted">Made by: <a href="https://github.com/mukundbhudia">Mukund</a>, <a href="https://github.com/salomao-rodrigues">Sal</a></span>
-            </div>
-          </footer>
+          <InnerPage
+            id="global-page"
+            title="Global"
+            totalCases={totalCases}
+            lastUpdated={lastUpdated}
+            globalTimeSeries={globalTimeSeries}
+            topXconfirmedByCountry={topXconfirmedByCountry}
+            topXactiveByCountry={topXactiveByCountry}
+            topXrecoveredByCountry={topXrecoveredByCountry}
+            topXdeathsByCountry={topXdeathsByCountry}
+          />
         </main>
       </div>
     </div>
