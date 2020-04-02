2f25879a81135814db2ad35bbbf2d1edb73e63f0
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-04-02 16:49:02 +0100

    Updated data sources in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,15 @@ const Footer = () => {
     <>
       <footer className="footer col-md-10 float-right mt-auto py-3">
         <div className="container pull-left">
-            <span className="text-muted">Data sources: <a href="https://www.who.int/">WHO</a>, <a href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6">John Hopkins University</a></span>
+          <span className="text-muted">
+            Data sources: <a href="https://github.com/CSSEGISandData/COVID-19">JHU CSSE</a>,&nbsp;
+            <a href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6">John Hopkins University</a>&nbsp;
+          </span>
         </div>
         <div className="container pull-right">
-            <span className="text-muted">Made by: <a href="https://github.com/mukundbhudia">Mukund</a>, <a href="https://github.com/salomao-rodrigues">Sal</a></span>
+          <span className="text-muted">Made by: <a href="https://github.com/mukundbhudia">Mukund</a>,&nbsp;
+            <a href="https://github.com/salomao-rodrigues">Sal</a>&nbsp;
+          </span>
         </div>
       </footer>
     </>
