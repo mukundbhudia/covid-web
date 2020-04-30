ebd8aa90dca2f9433bb0ed9f6cf72a4a171aea61
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-04-30 18:55:45 +0100

    Added links to data table, mobile responsive and bolding of selected col

--- src/components/InnerPages/DataTableInnerPage.jsx
+++ src/components/InnerPages/DataTableInnerPage.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
+import {
+  Link,
+} from 'react-router-dom'
 import * as timeago from 'timeago.js'
 import DataUpdatedTimeStamp from '../Nav/DataUpdatedTimeStamp'
 
@@ -24,9 +27,9 @@ const DataTableInnerPage = ({ title, lastUpdated }) => {
   const [sortConfig, setSortConfig] = useState({ key: 'country', direction: 'ascending' })
 
   const { loading, error, data } = useQuery(getTopCases())
-
   if (loading) return <p>Loading data for dashboard ...</p>
   if (error) return <p>{JSON.stringify(error, null, 2)}</p>
+
   const tableData = data.casesByLocationWithNoProvince
 
   const sortedItems = () => {
@@ -44,7 +47,6 @@ const DataTableInnerPage = ({ title, lastUpdated }) => {
     }
     return sortableItems
   }
-
 
   const requestSort = (key) => {
     let direction = 'ascending'
@@ -66,6 +68,14 @@ const DataTableInnerPage = ({ title, lastUpdated }) => {
     return indicator
   }
 
+  const applySelectedColumnStyle = (headerKey) => {
+    let style = ''
+    if (sortConfig.key === headerKey) {
+      style = 'selectedForSortColumn'
+    }
+    return style
+  }
+
   let sortedTableData = sortedItems(tableData)
 
   return (
@@ -79,7 +89,7 @@ const DataTableInnerPage = ({ title, lastUpdated }) => {
       </div>
 
       <div className="row">
-        <div className="col-sm">
+        <div className="col-sm table-responsive">
           <table className="table table-sm table-hover sortable-data-table">
             <thead className="thead-light">
               <tr>
@@ -97,14 +107,14 @@ const DataTableInnerPage = ({ title, lastUpdated }) => {
               {sortedTableData.map((tableRow) => {
                 return (
                   <tr key={tableRow.idKey}>
-                    <th scope="row">{tableRow.country}</th>
-                    <td>{tableRow.confirmed}</td>
-                    <td>{tableRow.confirmedCasesToday}</td>
-                    <td>{tableRow.active}</td>
-                    <td>{tableRow.recovered}</td>
-                    <td>{tableRow.deaths}</td>
-                    <td>{tableRow.deathsToday}</td>
-                    <td>{timeago.format(new Date(parseInt(tableRow.lastUpdate)))}</td>
+                    <th className={applySelectedColumnStyle('country')} scope="row"><Link to={tableRow.idKey}>{tableRow.country}</Link></th>
+                    <td className={applySelectedColumnStyle('confirmed')}>{tableRow.confirmed}</td>
+                    <td className={applySelectedColumnStyle('confirmedCasesToday')}>{tableRow.confirmedCasesToday}</td>
+                    <td className={applySelectedColumnStyle('active')}>{tableRow.active}</td>
+                    <td className={applySelectedColumnStyle('recovered')}>{tableRow.recovered}</td>
+                    <td className={applySelectedColumnStyle('deaths')}>{tableRow.deaths}</td>
+                    <td className={applySelectedColumnStyle('deathsToday')}>{tableRow.deathsToday}</td>
+                    <td className={applySelectedColumnStyle('lastUpdate')}>{timeago.format(new Date(parseInt(tableRow.lastUpdate)))}</td>
                   </tr>
                 )
               })}
