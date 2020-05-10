a166ba24e7e9242442220b4eab12b46701d1e45e
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-05-10 11:01:31 +0100

    Updates

---
+++ precache-manifest.720bbdfb6591d62cb949f9a45862e097.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35db407d76f08f5b73bb25e06c534124",
+    "url": "/mukundbhudia/covid-web/index.html"
+  },
+  {
+    "revision": "bed28768d6fc324b046e",
+    "url": "/mukundbhudia/covid-web/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "aeb300182381ad50180e",
+    "url": "/mukundbhudia/covid-web/static/css/main.4402605b.chunk.css"
+  },
+  {
+    "revision": "bed28768d6fc324b046e",
+    "url": "/mukundbhudia/covid-web/static/js/2.e9c011b5.chunk.js"
+  },
+  {
+    "revision": "5dbfb3036da28e985879c4b20b7ac4a1",
+    "url": "/mukundbhudia/covid-web/static/js/2.e9c011b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb300182381ad50180e",
+    "url": "/mukundbhudia/covid-web/static/js/main.2e8b6f12.chunk.js"
+  },
+  {
+    "revision": "e212bbf28eab4cb71b5c",
+    "url": "/mukundbhudia/covid-web/static/js/runtime-main.9f558eba.js"
+  }
+]);
