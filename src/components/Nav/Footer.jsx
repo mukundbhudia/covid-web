31daffd7568e9a06c5b37c20b64bb034aed38c7d
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-07-25 12:03:47 +0100

    Updated footer

--- src/components/Nav/Footer.jsx
+++ src/components/Nav/Footer.jsx
@@ -7,13 +7,13 @@ const Footer = () => {
       <footer className="footer col-md-10 float-right mt-5 py-3">
         <div className="container pull-left">
           <span className="text-muted">
-            Data sources: <a href="https://github.com/CSSEGISandData/COVID-19">JHU CSSE</a>,&nbsp;
-            <a href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6">John Hopkins University</a>&nbsp;
+            Data sources: <a target="_blank" rel="noopener noreferrer" href="https://github.com/CSSEGISandData/COVID-19">JHU CSSE</a>,&nbsp;
+            <a target="_blank" rel="noopener noreferrer" href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6">John Hopkins University</a>.&nbsp;
           </span>
         </div>
         <div className="container pull-right">
-          <span className="text-muted">Made by: <a href="https://github.com/mukundbhudia">Mukund</a>,&nbsp;
-            <a href="https://github.com/salomao-rodrigues">Sal</a>&nbsp;
+          <span className="text-muted">Made by: <a target="_blank" rel="noopener noreferrer" href="https://github.com/mukundbhudia">Mukund</a>,&nbsp;
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/salomao-rodrigues">Salomão</a>.&nbsp;
           </span>
         </div>
       </footer>
