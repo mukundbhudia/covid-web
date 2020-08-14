1f41110513709b019898a627ccb63adfa0d17a68
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-08-14 12:11:51 +0100

    Fixed bug for countries with no reported deaths

--- src/components/Panels/PanelNotableDates.jsx
+++ src/components/Panels/PanelNotableDates.jsx
@@ -4,13 +4,18 @@ import * as timeago from 'timeago.js'
 const PanelNotableDates = ({ date, message }) => {
   const notableDate = new Date(date)
   const timeAgoDate = timeago.format(date)
+  const renderedDate = (
+    <>
+      { notableDate.toLocaleDateString() }
+      <span> ({ timeAgoDate })</span>
+    </>
+  )
 
   return (
     <>
       <div className="alert alert-notable" role="alert">
         <div className="notable-dates text-notable">
-          <strong>{ message }</strong>: { notableDate.toLocaleDateString() }
-          <span> ({ timeAgoDate })</span>
+          <strong>{ message }</strong>: { date ? renderedDate : 'N/A' }
         </div>
       </div> 
     </>
