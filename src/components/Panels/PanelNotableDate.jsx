d271ff681d5ff710b9d64ca0b2a1672eae77c6e5
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-11-07 11:14:30 +0000

    feat: added tooltips to show days since first case

--- src/components/Panels/PanelNotableDate.jsx
+++ src/components/Panels/PanelNotableDate.jsx
@@ -3,20 +3,33 @@ import * as timeago from 'timeago.js'
 
 const PanelNotableDate = ({ data, message }) => {
   const notableDate = new Date(data.day)
+  const today = new Date()
   const timeAgoDate = timeago.format(data.day)
+  const timeAgoInDays = Math.trunc(
+    (today.getTime() - notableDate.getTime()) / (1000 * 3600 * 24)
+  )
+
   let renderedDate = (
     <>
-      { notableDate.toLocaleDateString() }
-      <span> ({ timeAgoDate })</span>
+      {notableDate.toLocaleDateString()}
+      &nbsp;
+      <span className="notable-tooltip" data-tip={`${timeAgoInDays} days ago`}>
+        ({timeAgoDate})
+      </span>
     </>
   )
 
   if (message === 'Most cases' || message === 'Most deaths') {
     renderedDate = (
       <>
-        { data.count ? data.count.toLocaleString() : 'N/A' }
+        {data.count ? data.count.toLocaleString() : 'N/A'}
         &nbsp;
-        <span className="notable-tooltip" data-tip={ notableDate.toLocaleDateString() }>({ timeAgoDate })</span>
+        <span
+          className="notable-tooltip"
+          data-tip={notableDate.toLocaleDateString()}
+        >
+          ({timeAgoDate})
+        </span>
       </>
     )
   }
@@ -24,9 +37,9 @@ const PanelNotableDate = ({ data, message }) => {
   return (
     <div className="alert alert-notable" role="alert">
       <div className="notable-dates text-notable">
-        <strong>{ message }</strong>: { data.day ? renderedDate : 'N/A' }
+        <strong>{message}</strong>: {data.day ? renderedDate : 'N/A'}
       </div>
-    </div> 
+    </div>
   )
 }
 
