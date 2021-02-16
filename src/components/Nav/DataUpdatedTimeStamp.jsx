59add9f6784b85b4c51905297124169f4c6a8c18
Mukund Bhudia <mukund_bhudia@hotmail.com>
2021-02-16 20:14:34 +0000

    feat: show unkown as time stamp if unix time is 0

--- src/components/Nav/DataUpdatedTimeStamp.jsx
+++ src/components/Nav/DataUpdatedTimeStamp.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import * as timeago from 'timeago.js'
 
 const UpdatedTimeStamp = ({ lastUpdated }) => {
-  const lastUpdatedDate = timeago.format(new Date(parseInt(lastUpdated)))
+  const dataAsInt = parseInt(lastUpdated)
+  let lastUpdatedDate = timeago.format(new Date(dataAsInt))
+  if (lastUpdated <= 0) {
+    lastUpdatedDate = "unknown"
+  }
   return (
     <div className="col-sm float-right">
       <p className="lastUpdatedTimeStamp float-right">Data last updated: <span id="lastUpdated">{lastUpdatedDate}</span></p>
