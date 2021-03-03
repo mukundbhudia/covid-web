ce3826acbef403d3c11a1bf8e1499257accae56d
Mukund Bhudia <mukund_bhudia@hotmail.com>
2021-03-03 17:33:11 +0000

    feat: added panels and bar charts to page

--- src/components/InnerPages/VaccinationsInnerPage.jsx
+++ src/components/InnerPages/VaccinationsInnerPage.jsx
@@ -5,8 +5,13 @@ import ErrorInnerPage from './ErrorInnerPage'
 import LoadingInnerPage from './LoadingInnerPage'
 import DataUpdatedTimeStamp from '../Nav/DataUpdatedTimeStamp'
 import { getVaccinationData } from '../../modules/queries'
+import { calculatePercentageWithDp } from '../../modules/numeric'
 import RadarChart from '../Charts/RadarChart'
 import TopXBarGraph from '../Charts/TopXBarGraph'
+import PanelPeopleFullyVaccinated from '../Panels/PanelPeopleFullyVaccinated'
+import PanelTotalVaccinations from '../Panels/PanelTotalVaccinations'
+
+const MAX_COUNTRIES_TO_SHOW = 19
 
 const VaccinationsInnerPage = ({ lastUpdated }) => {
   const { loading, error, data } = useQuery(getVaccinationData)
@@ -16,8 +21,6 @@ const VaccinationsInnerPage = ({ lastUpdated }) => {
 
   const continents = new Map()
 
-  console.log(data)
-
   data.casesByLocationWithNoProvince.forEach((country) => {
     let continent = country.continent
     let foundContinent = continents.get(continent)
@@ -25,37 +28,69 @@ const VaccinationsInnerPage = ({ lastUpdated }) => {
       if (foundContinent) {
         foundContinent.totalVaccinations += country.totalVaccinations
         foundContinent.peopleFullyVaccinated += country.peopleFullyVaccinated
+        foundContinent.peopleVaccinated += country.peopleVaccinated
+        foundContinent.population += country.population
+        foundContinent.percentOfPeopleVaccinated = calculatePercentageWithDp(
+          foundContinent.peopleVaccinated,
+          foundContinent.population,
+          2
+        )
+        foundContinent.percentOfPeopleFullyVaccinated = calculatePercentageWithDp(
+          foundContinent.peopleFullyVaccinated,
+          foundContinent.population,
+          2
+        )
         continents.set(continent, foundContinent)
       } else {
         continents.set(continent, {
           totalVaccinations: 0,
           peopleFullyVaccinated: 0,
+          peopleVaccinated: 0,
+          population: 0,
+          percentOfPeopleVaccinated: 0,
+          percentOfPeopleFullyVaccinated: 0,
         })
       }
     }
   })
 
-  let percentBar = data.casesByLocationWithNoProvince
-    .filter((country) => {
+  const nonNullVaccinatedCountries = data.casesByLocationWithNoProvince.filter(
+    (country) => {
       return (
         country.totalVaccinations &&
         country.peopleVaccinated &&
         country.peopleFullyVaccinated
       )
-    })
+    }
+  )
+
+  const percentOfPeopleFullyVaccinated = nonNullVaccinatedCountries
     .map((country) => {
       return {
         country: country.country,
-        percentOfPeopleFullyVaccinated: (
-          (country.peopleFullyVaccinated / country.population) *
-          100
-        ).toFixed(2),
+        percentOfPeopleFullyVaccinated: calculatePercentageWithDp(
+          country.peopleFullyVaccinated,
+          country.population,
+          2
+        ),
       }
     })
     .sort((a, b) => {
       return b.percentOfPeopleFullyVaccinated - a.percentOfPeopleFullyVaccinated
     })
-    .slice(0, 19)
+    .slice(0, MAX_COUNTRIES_TO_SHOW)
+
+  const totalVaccinations = nonNullVaccinatedCountries
+    .map((country) => {
+      return {
+        country: country.country,
+        totalVaccinations: country.totalVaccinations,
+      }
+    })
+    .sort((a, b) => {
+      return b.totalVaccinations - a.totalVaccinations
+    })
+    .slice(0, MAX_COUNTRIES_TO_SHOW)
 
   return (
     <>
@@ -69,6 +104,28 @@ const VaccinationsInnerPage = ({ lastUpdated }) => {
 
       <div className="row mb-4">
         <div className="col-sm">
+          <PanelTotalVaccinations
+            title="Total global vaccinations"
+            data={data.totalCases.totalVaccinations}
+          />
+          <PanelPeopleFullyVaccinated
+            title="% of people vaccinated"
+            data={data.totalCases.peopleVaccinated}
+            population={data.totalCases.population}
+          />
+        </div>
+        <div className="col-sm">
+          <PanelTotalVaccinations
+            title="Total vaccinations per hundred"
+            data={data.totalCases.totalVaccinationsPerHundred}
+          />
+          <PanelPeopleFullyVaccinated
+            title="% of people fully vaccinated"
+            data={data.totalCases.peopleFullyVaccinated}
+            population={data.totalCases.population}
+          />
+        </div>
+        <div className="col-sm-7">
           <RadarChart
             data={continents}
             id="vaccinationsContinent"
@@ -83,7 +140,20 @@ const VaccinationsInnerPage = ({ lastUpdated }) => {
       <div className="row mb-4">
         <div className="col-sm">
           <TopXBarGraph
-            data={percentBar}
+            data={totalVaccinations}
+            id="totalVaccinations"
+            chartTitle="Total vaccinations"
+            chartLabel="Total vaccinations"
+            chartLabelKey="totalVaccinations"
+            labelColor="purple"
+          />
+        </div>
+      </div>
+
+      <div className="row mb-4">
+        <div className="col-sm">
+          <TopXBarGraph
+            data={percentOfPeopleFullyVaccinated}
             id="percentOfPeopleFullyVaccinated"
             chartTitle="Percentage of people fully vaccinated"
             chartLabel="Percentage of people fully vaccinated"
