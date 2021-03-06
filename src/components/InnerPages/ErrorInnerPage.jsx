e010e2ee932a69007328605d30491cf35cd99479
Mukund Bhudia <mukund_bhudia@hotmail.com>
2021-03-06 11:20:23 +0000

    chore: applied formatter

--- src/components/InnerPages/ErrorInnerPage.jsx
+++ src/components/InnerPages/ErrorInnerPage.jsx
@@ -5,17 +5,31 @@ const ErrorInnerPage = ({ errorData }) => {
     <>
       <div className="container-fluid">
         <h3>Error</h3>
-        <p>There's been an error with COVID-19 Dashboard :(. <br/> Below are the details:</p>
+        <p>
+          There's been an error with COVID-19 Dashboard :(. <br /> Below are the
+          details:
+        </p>
         <div className="row">
           <div className="col-sm">
             <div className="card bg-light error-border border-danger mb-3">
               <div className="card-body">
-                <pre className="error">{JSON.stringify(errorData, null, 2)}</pre>
+                <pre className="error">
+                  {JSON.stringify(errorData, null, 2)}
+                </pre>
               </div>
             </div>
           </div>
         </div>
-        <p>Click <span className="btn btn-link span-link" onClick={() => window.location.reload()}>here</span> to re-load the page and try again.</p>
+        <p>
+          Click{' '}
+          <span
+            className="btn btn-link span-link"
+            onClick={() => window.location.reload()}
+          >
+            here
+          </span>{' '}
+          to re-load the page and try again.
+        </p>
       </div>
     </>
   )
